4413673858fbd6cc0170fb76637d5cd3c235a54c
Kavfox <kavcoffeefox@yandex.ru>
2023-06-19 19:07:53 +0300

    Implement doGet method in MealServlet

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.topjava.web;
 
+import ru.javawebinar.topjava.dao.MealDAO;
+import ru.javawebinar.topjava.dao.MealDAOInMemoryImpl;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.MealTo;
 
@@ -8,32 +10,50 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.time.Month;
-import java.util.Arrays;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 
 import static ru.javawebinar.topjava.util.MealsUtil.filteredByStreams;
 
 public class MealServlet extends HttpServlet {
+    public static final String MEAL_FORM_JSP = "meal_form.jsp";
+    public static final String MEALS_LIST = "meals.jsp";
+    private final MealDAO mealDAO = MealDAOInMemoryImpl.getInstance();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        List<Meal> meals = Arrays.asList(
-                new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
-                new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000),
-                new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500),
-                new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100),
-                new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000),
-                new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500),
-                new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
-        );
+        String forward="";
+        String action = req.getParameter("action");
 
-        List<MealTo> mealsTo = filteredByStreams(meals, LocalTime.of(0, 0), LocalTime.of(23, 59), 2000);
+        if (Objects.isNull(action)) {
+            List<Meal> meals = mealDAO.findAll();
+            List<MealTo> mealsTo = filteredByStreams(meals, LocalTime.of(0, 0), LocalTime.of(23, 59), 2000);
 
-        req.setAttribute("meals", mealsTo);
+            req.setAttribute("meals", mealsTo);
+            forward = MEALS_LIST;
+        } else if (action.equalsIgnoreCase("delete")){
+            int userId = Integer.parseInt(req.getParameter("mealId"));
+            mealDAO.deleteById(userId);
+            forward = MEALS_LIST;
+            List<Meal> meals = mealDAO.findAll();
+            List<MealTo> mealsTo = filteredByStreams(meals, LocalTime.of(0, 0), LocalTime.of(23, 59), 2000);
+            req.setAttribute("meals", mealsTo);
+        } else if (action.equalsIgnoreCase("edit")){
+            forward = MEAL_FORM_JSP;
+            int mealId = Integer.parseInt(req.getParameter("mealId"));
+            Meal meal = mealDAO.findById(mealId).orElseThrow(NoSuchElementException::new);
+            req.setAttribute("meal", meal);
+        } else if (action.equalsIgnoreCase("insert")){
+            forward = MEAL_FORM_JSP;
 
-        req.getRequestDispatcher("meals.jsp").forward(req,resp);
+        }
+        req.getRequestDispatcher(forward).forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
     }
 }
